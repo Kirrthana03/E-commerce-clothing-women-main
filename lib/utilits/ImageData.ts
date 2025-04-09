0a53ccbe6ee3f1d41639984dd5bbbcf832dbf57f
Kirrthana03 <kirrthana.setiahorizon@gmail.com>
2025-04-09 19:16:02 +0800

    Add files via upload

---
+++ lib/utilits/ImageData.ts
@@ -0,0 +1,30 @@
+import imageBlouse from "@/public/collection/Lifestyle_Detail_Something_Tailored_Shirt_White_1400x.webp";
+import imagePants from "@/public/collection/Moodboard2_71ade389-dc80-49eb-b7e8-1c90a0273a2a_700x.webp";
+import imageDress from "@/public/collection/Save_The_Date_Dress_Khaki_Lifestyle_Khaki_Main_720x.webp";
+import imageOutwear from "@/public/collection/ezgif-2-f137fd9d7d.png";
+export const ImagesMansory = [
+  {
+    src: imageBlouse,
+    height: 400,
+    name: "Boluses",
+    id: 0,
+  },
+  {
+    src: imagePants,
+    height: 700,
+    name: "Pants",
+    id: 1,
+  },
+  {
+    src: imageDress,
+    height: 600,
+    name: "Dresses",
+    id: 2,
+  },
+  {
+    src: imageOutwear,
+    height: 300,
+    name: "Outwear",
+    id: 3,
+  },
+];
